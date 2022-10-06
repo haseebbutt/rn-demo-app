2069bd7dbea84ab2b6f71613cf5c083a241e032e
haseebbutt <haseebbutt687@gmail.com>
2022-10-06 16:10:17 +0500

    added all files

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
+// import {View,Text  } from 'react-native'
 import Stack from './src/stack';
 
 //
@@ -8,6 +8,8 @@ import Stack from './src/stack';
 
 const App = () => {
   return (
+    // <View><Text>APP</Text></View>
+
     <NavigationContainer>
       <Stack />
     </NavigationContainer>
